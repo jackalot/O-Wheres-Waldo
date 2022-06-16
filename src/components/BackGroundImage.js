a6a5cc46743a7397101974cd562163b8445da38a
Jackalot <gonzalezc9999@gmail.com>
2022-06-16 08:55:27 -0400

    added a div and image to BackgroundIMage to be returned

--- src/components/BackGroundImage.js
+++ src/components/BackGroundImage.js
@@ -1,3 +1,9 @@
 import React from "react";
-function BackGroundImage() {}
+function BackGroundImage() {
+  return (
+    <div>
+      <img></img>
+    </div>
+  );
+}
 export default BackGroundImage;
