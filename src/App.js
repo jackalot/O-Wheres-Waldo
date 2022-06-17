cd0adcae98b7082fe7b74ee1c342ede85ac5bee7
Jackalot <gonzalezc9999@gmail.com>
2022-06-17 09:41:27 -0400

    added useState hooks, added mousePosition as a state variabel

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import "./App.css";
 import SelectedArea from "./components/SelectedArea";
 import BackgroundImage from "./components/BackGroundImage";
+import { useState } from "react";
 function App() {
+  const [mousePosition, setMousePosition] = useState();
   return (
     <div className="App">
       <SelectedArea></SelectedArea>
