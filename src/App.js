21892fd38f23d172c951e8ed6e19ba2cb4b880c0
Jackalot <gonzalezc9999@gmail.com>
2022-06-17 09:28:53 -0400

    background image is imported to app.js and rendered

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
 import SelectedArea from "./components/SelectedArea";
+import BackgroundImage from "./components/BackGroundImage";
 function App() {
   return (
     <div className="App">
       <SelectedArea></SelectedArea>
+      <BackgroundImage></BackgroundImage>
     </div>
   );
 }
