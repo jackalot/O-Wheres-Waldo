e0393385794a08077c1722a3e7fd5362c030101d
Jackalot <gonzalezc9999@gmail.com>
2022-06-21 10:05:14 -0400

    added a snapshot test

--- src/test-suites-components/SelectedArea.test.js
+++ src/test-suites-components/SelectedArea.test.js
@@ -1,6 +1,9 @@
-import { render, screen } from "@testing-library/react";
+import { getByRole, render, screen } from "@testing-library/react";
 import SelectedArea from "../components/SelectedArea";
 
 describe("SelectedArea component", () => {
-  test("Selected Area div is rendered", () => {});
+  test("Selected Area div is rendered", () => {
+    const { container } = render(<SelectedArea></SelectedArea>);
+    expect(container).toMatchSnapshot();
+  });
 });
