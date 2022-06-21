432ed90fdbb5302a3a574c89f4ffdc96669e578e
Jackalot <gonzalezc9999@gmail.com>
2022-06-21 09:52:23 -0400

    added a new test that checks if the SelectedArea div is rendered, test is empty

--- src/test-suites-components/SelectedArea.test.js
+++ src/test-suites-components/SelectedArea.test.js
@@ -1,4 +1,6 @@
 import { render, screen } from "@testing-library/react";
 import SelectedArea from "../components/SelectedArea";
 
-describe("SelectedArea component", () => {});
+describe("SelectedArea component", () => {
+  test("Selected Area div is rendered", () => {});
+});
