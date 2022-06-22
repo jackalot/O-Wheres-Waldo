9f1da57c40aab08d0d1e0c9fd1324b5c94b3ac49
Jackalot <gonzalezc9999@gmail.com>
2022-06-22 09:37:26 -0400

    created a sample test in App component so that the App Test suite doesn't fail

--- src/App.test.js
+++ src/App.test.js
@@ -1,2 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
+describe("App Component", () => {
+  it("sample test to make this test suit not fail", () => {
+    expect(2 + 2).toBe(4);
+  });
+});
