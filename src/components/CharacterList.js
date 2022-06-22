71c6798fd6377e96f47799bc2873b02a7fc6f15e
Jackalot <gonzalezc9999@gmail.com>
2022-06-22 08:39:20 -0400

    removed the li elements

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -2,11 +2,7 @@ import React from "react";
 function CharacterList(props) {
   return (
     <div className="character-list">
-      <ul>
-        <li>{props.character1}</li>
-        <li>{props.character2}</li>
-        <li>{props.character3}</li>
-      </ul>
+      <ul></ul>
     </div>
   );
 }
