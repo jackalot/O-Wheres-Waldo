ab245cf84e04f749bad3154a4aac3d7acf0788e9
Jackalot <gonzalezc9999@gmail.com>
2022-06-22 08:39:57 -0400

    added a map function that sorts through an 'ourCharacters' array

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -2,7 +2,11 @@ import React from "react";
 function CharacterList(props) {
   return (
     <div className="character-list">
-      <ul></ul>
+      <ul>
+        {props.ourCharacters.map((character, char) => (
+          <li key={char}>{character.name}</li>
+        ))}
+      </ul>
     </div>
   );
 }
