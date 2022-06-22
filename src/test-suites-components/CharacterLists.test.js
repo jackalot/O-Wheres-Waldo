80c1812322d2e6bb86aa9d84d9befc41a1240b10
Jackalot <gonzalezc9999@gmail.com>
2022-06-22 09:40:10 -0400

    created another test for CharacterLists

--- src/test-suites-components/CharacterLists.test.js
+++ src/test-suites-components/CharacterLists.test.js
@@ -17,4 +17,13 @@ describe("CharacterList component", () => {
     const johnTest = getByText("John");
     expect(johnTest.textContent).toContain("John");
   });
+  it("John and Dave both render", () => {
+    const { getByText } = render(
+      <CharacterList ourCharacters={characterArray}></CharacterList>
+    );
+    const johnTest = getByText("John");
+    expect(johnTest.textContent).toContain("John");
+    const DaveTest = getByText("Dave");
+    expect(DaveTest.textContent).toContain("Dave");
+  });
 });
