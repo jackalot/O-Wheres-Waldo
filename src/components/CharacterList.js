ecbd7fcc90b48977fede857593df8d6f10f0f61d
Jackalot <gonzalezc9999@gmail.com>
2022-06-22 10:27:26 -0400

    gave CharacterList the same inline style as SelectedArea

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -2,7 +2,14 @@ import React from "react";
 import "../css-components/CharacterList.css";
 function CharacterList(props) {
   return (
-    <div className="character-list">
+    <div
+      style={{
+        position: "absolute",
+        left: `${props.xPosition}px`,
+        top: `${props.yPosition}px`,
+      }}
+      className="character-list"
+    >
       <ul>
         {props.ourCharacters.map((character, char) => (
           <li key={char}>{character.name}</li>
