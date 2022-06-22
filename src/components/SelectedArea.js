2340f11ee683d089239e8af14215fbc1fb10c4a1
Jackalot <gonzalezc9999@gmail.com>
2022-06-22 10:18:59 -0400

    moved the inline style to the innerContainer

--- src/components/SelectedArea.js
+++ src/components/SelectedArea.js
@@ -2,15 +2,15 @@ import React from "react";
 import "../css-components/SelectedArea.css";
 function SelectedArea(props) {
   return (
-    <div
-      style={{
-        position: "absolute",
-        left: `${props.xPosition}px`,
-        top: `${props.yPosition}px`,
-      }}
-      className="selectedArea-OuterContainer"
-    >
-      <div className="selectedArea-InnerContainer"></div>
+    <div className="selectedArea-OuterContainer">
+      <div
+        style={{
+          position: "absolute",
+          left: `${props.xPosition}px`,
+          top: `${props.yPosition}px`,
+        }}
+        className="selectedArea-InnerContainer"
+      ></div>
     </div>
   );
 }
