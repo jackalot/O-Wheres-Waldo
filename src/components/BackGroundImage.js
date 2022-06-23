a651334135c4024790f2af6487994f44ccb7ba60
Jackalot <gonzalezc9999@gmail.com>
2022-06-23 10:23:19 -0400

    gave the img tag in BackGroundImage the classname attribute and gave it the class of backGround

--- src/components/BackGroundImage.js
+++ src/components/BackGroundImage.js
@@ -4,6 +4,7 @@ function BackGroundImage() {
   return (
     <div>
       <img
+        className="backgroundImage"
         src={require("../images/TheVoidIsCallingByAfros2000.jpg")}
         alt="The Birds, The Bees, & The Monkees by Dukepinball"
       ></img>
