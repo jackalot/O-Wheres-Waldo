9b3d9c354fffd00ef42fd8a99d1a95693c112b36
Jackalot <gonzalezc9999@gmail.com>
2022-06-23 10:18:17 -0400

    the old background image has been replace, and deleted from the project. BackGroundImage now uses the new image

--- src/components/BackGroundImage.js
+++ src/components/BackGroundImage.js
@@ -3,7 +3,7 @@ function BackGroundImage() {
   return (
     <div>
       <img
-        src={require("../images/BirdsBeesMonkeesByDukepinball.png")}
+        src={require("../images/TheVoidIsCallingByAfros2000.jpg")}
         alt="The Birds, The Bees, & The Monkees by Dukepinball"
       ></img>
     </div>
