6aff6a39ed9c16387df4e6cadab135bc4c459415
Jackalot <gonzalezc9999@gmail.com>
2022-07-05 10:47:48 -0400

    added a test that checks for the StartGame Button and if it renders

--- src/test-suites-components/StartMenu.test.js
+++ src/test-suites-components/StartMenu.test.js
@@ -1,4 +1,8 @@
 import { getByRole, render, screen } from "@testing-library/react";
 import StartMenu from "../components/StartMenu";
 
-describe("StartMenu component", () => {});
+describe("StartMenu component", () => {
+  it("The StartGame button renders", () => {
+    const { getByRole } = render(<StartMenu></StartMenu>);
+  });
+});
