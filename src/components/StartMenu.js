6238f627ce45827e5aa4d49267a949795dc6bb7d
Jackalot <gonzalezc9999@gmail.com>
2022-07-05 09:58:55 -0400

    created two elements, a p and a button. The p has the classname 'HowToPlayText' the button has 'StartButton'

--- src/components/StartMenu.js
+++ src/components/StartMenu.js
@@ -3,7 +3,10 @@ import React from "react";
 function StartMenu() {
   return (
     <div className="StartMenu-container">
-      <div className="StartMenu-ButtonContainer"></div>
+      <div className="StartMenu-ButtonContainer">
+        <p className="StartMenu-HowToPlayText"></p>
+        <button className="StartMenu-StartBtn">Start!</button>
+      </div>
     </div>
   );
 }
