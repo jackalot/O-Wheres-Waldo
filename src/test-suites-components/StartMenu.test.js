1d8e3af17dbdef0eca625811d5e3dfc88b315b08
Jackalot <gonzalezc9999@gmail.com>
2022-07-05 10:50:47 -0400

    created a characterArray

--- src/test-suites-components/StartMenu.test.js
+++ src/test-suites-components/StartMenu.test.js
@@ -2,6 +2,16 @@ import { getByRole, render, screen } from "@testing-library/react";
 import StartMenu from "../components/StartMenu";
 
 describe("StartMenu component", () => {
+  const characterArray = [
+    {
+      charName: "Bear",
+      charDir: "../images/Bear.png",
+    },
+    {
+      name: "Bird",
+      charDir: "../images/Bird.png",
+    },
+  ];
   it("The StartGame button renders", () => {
     const { getByRole } = render(<StartMenu></StartMenu>);
   });
