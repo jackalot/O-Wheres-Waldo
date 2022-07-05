21bd140f7fb60217563ac76de6239ad7f387e66c
Jackalot <gonzalezc9999@gmail.com>
2022-07-05 10:00:52 -0400

    Added instructions to the 'HowToPlayText' element

--- src/components/StartMenu.js
+++ src/components/StartMenu.js
@@ -4,7 +4,10 @@ function StartMenu() {
   return (
     <div className="StartMenu-container">
       <div className="StartMenu-ButtonContainer">
-        <p className="StartMenu-HowToPlayText"></p>
+        <p className="StartMenu-HowToPlayText">
+          The images on the right will be spread throughout the image you'll see
+          after you click 'Start!', good luck and have fun!
+        </p>
         <button className="StartMenu-StartBtn">Start!</button>
       </div>
     </div>
