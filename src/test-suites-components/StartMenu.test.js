e0240e0c51a1863f45eecb1976b75e047e2c48d4
Jackalot <gonzalezc9999@gmail.com>
2022-07-06 10:57:22 -0400

    removed the test entirely

--- src/test-suites-components/StartMenu.test.js
+++ src/test-suites-components/StartMenu.test.js
@@ -27,21 +27,6 @@ describe("StartMenu component", () => {
       true
     );
   });
-  it("A mock button hides the container div when clicked", () => {
-    function mockChangeClass() {
-      const container = document.getByRole(".Startmenu-container");
-      container.style.display = "none";
-    }
-    const { getByRole } = render(
-      <StartMenu ourCharacters={characterArray}></StartMenu>
-    );
-    /* We use a new, mock button instead of the Start button
-    as trying to test the start button would give errors with 
-    container.style.display */
-    const mockButton = createElement("button");
-    mockButton.onclick(mockChangeClass);
-    fireEvent.click(mockButton);
-  });
   it("The Bear image has the correct alt and src", () => {
     const { getByAltText } = render(
       <StartMenu ourCharacters={characterArray}></StartMenu>
